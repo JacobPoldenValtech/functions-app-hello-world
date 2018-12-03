3b0d7076202521c43a813ed88dac245c24c34359
Jacob Polden <jacob.polden@valtech.co.uk>
2018-12-03 15:27:04 +0000

    sudo

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 ZIP_NAME='function.zip'
 
-rm $ZIP_NAME
-zip -r $ZIP_NAME * -x "*.git*" -x "*obj*" -x "local.settings.json" -x "proxies.json" -x "*scripts*"
+sudo rm $ZIP_NAME
+sudo zip -r $ZIP_NAME * -x "*.git*" -x "*obj*" -x "local.settings.json" -x "proxies.json" -x "*scripts*"
